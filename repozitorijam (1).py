75a298653bd3d20f4238f5529d0ddb2db93f91fc
Jēkabs <78420033+JekabsSeglins@users.noreply.github.com>
2021-02-16 14:18:04 +0200

    Update repozitorijam (1).py

--- repozitorijam (1).py
+++ repozitorijam (1).py
@@ -4,27 +4,28 @@ print(5+6)
 print(5**3)
 #1)Andersone Linda Elva
 
-#2)Apsîtis Edvards
+#2)ApsĆ®tis Edvards
 
-#3)Èeròevièa Letîcija Lîna
+#3)Ć�erĆ²eviĆØa LetĆ®cija LĆ®na
 
-#4)Drozdova Miðelle
+#4)Drozdova MiĆ°elle
 
-#5)Karlinskis Renârs
+#5)Karlinskis RenĆ¢rs
 
 #6)Lukjanskis Miks Patriks
 
-#7)Lukðs Lauris
+#7)LukĆ°s Lauris
 
-#8)Mâlkalns Kristaps
+#8)MĆ¢lkalns Kristaps
 
-#9)Ruòìis Kristaps
+#9)RuĆ²Ć¬is Kristaps
 
-#10)Segliòð Jçkabs
+#10)SegliĆ²Ć° JĆ§kabs
 print("jekabs",8+9)
-#11)Serþante Agate
+print("jauns kods",3*4)
+#11)SerĆ¾ante Agate
 
-#12)Voiteðonoks Aleksandrs
+#12)VoiteĆ°onoks Aleksandrs
 
 #13)Zvaigzne Gustavs Henrijs
 
